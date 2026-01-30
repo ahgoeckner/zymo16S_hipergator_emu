4b125743c37bf1058499b6a18cd46de2c59f80c2
ahgoeckner <agoeckner@ufl.edu>
2026-01-30 14:08:47 -0500

    update genus

--- combine_emu_outputs.sh
+++ combine_emu_outputs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=emu_combine_1_test
+#SBATCH --job-name=emu_combine
 #SBATCH --account=duttonc
 #SBATCH --qos=duttonc
 #SBATCH --partition=hpg-turin
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=8G
 #SBATCH --time=2:00:00
-#SBATCH --output=/blue/duttonc/agoeckner/DOE_16S/99_logs/emu_combine_1_test.log
+#SBATCH --output=/blue/duttonc/agoeckner/DOE_16S/99_logs/emu_combine_batch1genus.log
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=agoeckner@ufl.edu
 
